2f4402891d440ce4e13060e8c12b62de0c04287f
Boglut Loredana <loredanaboglut31@gmail.com>
2025-05-14 13:51:59 +0300

    Update with Hotjar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home/Homescreen";
@@ -16,8 +16,14 @@ import UserDashboardLayout from "./Pages/UserDashboardLayout";
 import PublicLayout from "./Pages/PublicLayout";
 
 import About from "./Pages/Home/About";
+import { initHotjar } from "./utils/hotjar";
 
 function App() {
+
+  useEffect(()=>{
+    initHotjar();
+  },[]);
+
   return (
     <div>
        <Router>
